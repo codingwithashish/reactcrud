b7fda80fb29a15fa035f169e95469d48f93e20a6
brady.ashish <brady.ashish@rubicotech.in>
2021-12-13 14:23:15 +0530

    Form design using react js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React from 'react';
 import Button from '@mui/material/Button';
-import { Box  } from '@mui/system';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -8,10 +8,20 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { FormControl, RadioGroup, TextField, FormControlLabel, Radio} from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { FormControl, RadioGroup, TextField, FormControlLabel, Radio, FormLabel} from '@mui/material';
 import Checkbox from '@mui/material/Checkbox';
 import RightArrow from '@mui/icons-material/KeyboardArrowRight';
+
 function App() {
+  const [domain, setdomain] = React.useState('');
+
+  const handleChange = (event) => {
+    setdomain(event.target.value);
+  };
+
   return (
     <>
     <header className="container">
@@ -94,19 +104,45 @@ function App() {
             type="number"
             color="secondary"
           />
-
+          <TextField
+            fullWidth
+            label="Hire Date"
+            type="date"
+            color="secondary"
+            defaultValue="2021-12-13"
+            className="mt-3"
+          />
           <FormControl component="fieldset" className="mt-3">
+            <FormLabel component="legend" className="mb-0">Gender</FormLabel>
             <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
               <FormControlLabel value="male" control={<Radio />} label="Male" />
               <FormControlLabel value="female" control={<Radio />} label="Female" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
             </RadioGroup>
           </FormControl> 
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-helper-label">Domain</InputLabel>
+            <Select
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              value={domain}
+              label="Domain"
+              onChange={handleChange}
+              color="secondary"
+              fullWidth
+            >
+            <MenuItem value={10}>Frontend Developer</MenuItem>
+            <MenuItem value={20}>Backend Developer</MenuItem>
+            <MenuItem value={30}>Network And support</MenuItem>
+            <MenuItem value={40}>Project Cordinator</MenuItem>
+            <MenuItem value={50}>QA</MenuItem>
+          </Select>
+      </FormControl>
           <TextField
             fullWidth
             label="Address"
             type="text"
             color="secondary"
+            className="mt-3"
           />  
           <div class="form-check ps-0">
             <Checkbox defaultChecked color="secondary" className="ps-0"/>
@@ -122,65 +158,6 @@ function App() {
     </div>
   </div>
 </div>
-
- {/* modal Update EMP*/}
-{/* <!-- Modal --> */}
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title text-uppercase" id="staticBackdropLabel">Update Employee Records</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form method="POST">
-        <div class="modal-body">
-          <TextField
-            fullWidth
-            label="Name"
-            color="secondary"
-          />
-          <TextField
-            fullWidth
-            label="Email"
-            type="email"
-            color="secondary"
-            className="my-3"
-          />
-          <TextField
-            fullWidth
-            label="Phone"
-            type="number"
-            color="secondary"
-          />
-
-          <FormControl component="fieldset" className="mt-3">
-            <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
-              <FormControlLabel value="other" control={<Radio />} label="Other" />
-            </RadioGroup>
-          </FormControl> 
-          <TextField
-            fullWidth
-            label="Address"
-            type="text"
-            color="secondary"
-          />  
-          <div class="form-check ps-0">
-            <Checkbox defaultChecked color="secondary" className="ps-0"/>
-            <label class="form-check-label" for="invalidCheck">
-              Agree to terms and conditions
-            </label>
-          </div>      
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn bg-pink text-white">Update</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
- 
  </> 
  )}
 
