9c14b6414af42100ed55497191e7a89104ee6166
brady.ashish <brady.ashish@rubicotech.in>
2022-06-04 11:25:43 +0530

    change ui design

--- src/modal/TodoModal.jsx
+++ src/modal/TodoModal.jsx
@@ -1,27 +1,38 @@
 import React, {useState} from 'react';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { TextField, Button } from '@mui/material';
+import {TextField, Button, Box} from '@mui/material';
 import RightArrow from '@mui/icons-material/KeyboardArrowRight';
 
 const TodoModal = () => {
-    const [inputval, setnewinputval] = useState();
-    const [newitem, setnewitem] = useState([]);
+    const [inputval,
+        setnewinputval] = useState();
+    const [newitem,
+        setnewitem] = useState([]);
 
-    const todovalue = (event) =>{
-        setnewinputval(event.target.value);   
+    const todovalue = (event) => {
+        setnewinputval(event.target.value);
     }
 
     const submitTodoForm = (e) => {
         e.preventDefault();
-        setnewitem((prevdata)=>{
-            return [...prevdata , inputval];
+        setnewitem((prevdata) => {
+            return [
+                ...prevdata,
+                inputval
+            ];
         });
         setnewinputval('');
     }
 
-    const deleteIem = (id) =>{
-        console.log('deleted' + id);
+    const deleteIem = (id) => {
+        // console.log(`deleted ${id}`);
+
+        setnewitem((prevdata) => {
+            return prevdata.filter((newele, index) => {
+                return index !== id;
+            })
+        })
     }
     return ( <> <div
         class="modal fade"
@@ -31,15 +42,15 @@ const TodoModal = () => {
         tabindex="-1"
         aria-labelledby="todomodalLabel"
         aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+        <Box class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title text-uppercase" id="todomodalLabel">ADD EMPLOYEE TODO</h5>
-                    <button
+                    <Button
                         type="button"
                         class="btn-close"
                         data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                        aria-label="Close"></Button>
                 </div>
                 <form method="POST" onSubmit={submitTodoForm}>
                     <div class="modal-body">
@@ -52,11 +63,17 @@ const TodoModal = () => {
                         <div className="list-item-wrapper">
                             <ul className="list-unstyled mt-3">
                                 {newitem.map((data, index) => {
-                                    return ( <> <li className='list-item d-flex justify-content-between px-2 mb-2' key={index} id={index}>
-                                        <span>{data}</span>
+                                    return ( <> <li
+                                        className='list-item d-flex justify-content-between px-2 mb-2'
+                                        key={index}
+                                        id={index}>
+                                        <span>{index + 1} {data}</span>
                                         <div className="action-icon">
                                             {/* <a href=""><ModeEditIcon/></a> */}
-                                            <a href="#" className="text-danger" onClick={()=>{
+                                            <a
+                                                href="#"
+                                                className="text-danger"
+                                                onClick={() => {
                                                 deleteIem(index);
                                             }}><DeleteIcon/>
                                             </a>
@@ -74,9 +91,8 @@ const TodoModal = () => {
                     </div>
                 </form>
             </div>
-        </div>
+        </Box>
     </div> </>
-
     )
 }
 
